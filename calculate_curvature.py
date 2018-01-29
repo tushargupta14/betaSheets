707df3a8a8c53133cf58b52c87d6e7324ba95075
Tushar Gupta <tushariitkgp14@gmail.com>
2018-01-29 23:31:56 +0530

    Completed curvature values

--- calculate_curvature.py
+++ calculate_curvature.py
@@ -5,73 +5,7 @@ import json
 import numpy as np 
 import re
 import math
-def clean_residue(residue) :
-
-
-	clean_residue  = re.findall('^\d+',residue)
-
-	#print clean_residue,residue
-	if len(clean_residue)==0:
-		return int(residue)	
-	else :
-		return int(clean_residue[0])
-
-
-
-def get_residue_dict(path_to_backbone_info,pdb_name) :
-
-
-	backbone_file = open(path_to_backbone_info+pdb_name+"_backbone.txt")
-
-	residue_dict = {}
-
-
-	for line in backbone_file :
-
-		vals = line.rstrip("\n").split("\t")
-		
-		x = vals[5]
-		y = vals[6]
-		z = vals[7]
-		
-		residue = clean_residue(vals[0])
-		#print x,y,z
-		#residue_dict[vals[0]] = [float(vals[5]),float(vals[6]),float(vals[7])]
-		
-		## Checking if a valid float
-		try :
-                        x = float(x)
-                except :
-
-                        if x.count('-') == 2 or x.count('-')==1 :
-                                a = float(x.rsplit('-',1)[0])
-                                b = float('-'+x.rsplit('-',1)[1])
-                                c = float(y)
-                                #print x,y,z
-                                #print a,b,c
-                                residue_dict[residue] = [a,b,c]
-                                continue
-
-                try :
-                        y = float(y)
-                except :
-
-                        if y.count('-') == 2 or y.count('-')==1:
-                                a = x
-                                b = float(y.rsplit('-',1)[0])
-                                c = float('-'+y.rsplit('-',1)[1])
-                                residue_dict[residue] = [a,b,c]
-                                #print x,y,z
-                                #print a,b,c
-                                continue
-
-                residue_dict[residue] = [float(vals[5]),float(vals[6]),float(vals[7])]
-
-                #residue_dict[vals[0]] = [float(vals[5]),float(vals[6]),float(vals[7])]
-
-        backbone_file.close()
-        return residue_dict
-	
+from get_residue_dict import *
 
 def compute_curvature(residue_dict,residue_list,npoints) :
 
@@ -126,7 +60,7 @@ def calculate_data_points(residue_list,residue_dict,npoints,slide_value) :
 	return curvature_list
 
 
-def calculate_curvature(pdb_name,chain,sheet_dict) :
+"""def calculate_curvature(pdb_name,chain,sheet_dict) :
 
 	path_to_backbone_info = "data/backbone_info/"
 	path_to_output = "data/curvature/"
@@ -173,55 +107,4 @@ def calculate_curvature(pdb_name,chain,sheet_dict) :
 		
 	with open(path_to_output+pdb_name+"_"+chain+".json","wb+") as f :
                         json.dump(curvature_dict,f)
-
-def recurse_pdbs() :
-
-
-	path_to_pdb_list = "30k_pdb_list"
-
-	path_to_pdbs = "/home/twistgroup/pdb/"
-
-	path_to_json_files = "data/sheet_info/"
-	path_to_output = "data/curvature/"
-
-	file = open(path_to_pdb_list,"rb+")
-
-	#unique_pdb_set = set()
-	error =0
-	count =0
-
-	error_file = open("curvature_error.txt","wb+")
-	for line in file :
-
-		vals = line.split(" ")
-
-		if "IDs" in vals[0] :
-			continue
-
-		#print vals[0]
-
-		pdb_name = vals[0][:4]
-		chain =  vals[0][-1]
-		pdb_name = pdb_name.lower()
-
-		print pdb_name,"-",chain
-
-		## retrieving strand data
-		#sheet_dict = json.load(open(path_to_json_files+pdb_name+"_"+chain+".json","rb+"))	
-		
-		try :
-			sheet_dict = json.load(open(path_to_json_files+pdb_name+"_"+chain+".json","rb+"))
-			calculate_curvature(pdb_name,chain,sheet_dict)
-		except Exception as e :
-			print e
-                        error+=1
-                        error_file.write(str(e)+"\t"+pdb_name+"\t"+chain+"\n")
-                        continue
-		count+=1
-	print error,count
-
-if __name__ == "__main__" :
-
-
-
-	recurse_pdbs()
+"""
---
+++ get_curvature_info.py
@@ -0,0 +1,124 @@
+## Exdtracts the curvature values for each direction
+
+from calculate_curvature import *
+from get_residue_dict import *
+from collections import defaultdict
+def curvature_values_along(pdb_name,chain,sheet_dict,residue_dict):
+
+	curvature_dict = defaultdict(list)
+	for sheet,residue_list in sheet_dict.iteritems():
+
+		#print sheet,residue_list
+		curv_3 = calculate_data_points(residue_list,residue_dict,npoints = 3,slide_value = 1)
+
+		curv_5 = calculate_data_points(residue_list,residue_dict,npoints = 5,slide_value = 1)
+		#print curv_3,curv_5
+		curvature_dict[sheet] = [curv_3,curv_5]
+		
+
+	return curvature_dict
+
+def curvature_values_across(pdb_name,chain,sheet_dict,residue_dict):
+
+	curvature_dict = defaultdict(list)
+	for sheet,residue_list in sheet_dict.iteritems():
+
+		#print sheet,residue_list
+
+		if len(residue_list)==0 :
+			continue
+		start_residues = residue_list[0]
+		last_residues = residue_list[1]
+
+		curv_1 = calculate_data_points(start_residues,residue_dict,npoints = 3,slide_value = 1)
+
+		curv_2 = calculate_data_points(last_residues,residue_dict,npoints = 3,slide_value = 1)
+
+		curvature_dict[sheet] = [curv_1,curv_2]
+		
+		#print curv_1,curv_2
+	
+	return curvature_dict
+
+def recurse_pdbs() :
+
+
+	path_to_pdb_list = "all_beta_sheets"
+	path_to_backbone_info = "allBeta_data/backbone_info/"
+	#path_to_pdbs = "/home/twistgroup/pdb/"
+
+	path_to_json_files = "allBeta_data/sheets/"
+
+	path_to_output = "allBeta_data/curvature/"
+
+	file = open(path_to_pdb_list,"rb+")
+
+	#unique_pdb_set = set()
+	error =0
+	count =0
+
+	error_file = open("allBeta_curvature_error.txt","wb+")
+	for line in file :
+
+		vals = line.split(" ")
+
+		if "IDs" in vals[0] :
+			continue
+
+		#print vals[0]
+
+		pdb_name = vals[0][:4]
+		chain =  vals[0][-1]
+		pdb_name = pdb_name.lower()
+
+		print pdb_name,"-",chain	
+		
+		
+		try :
+
+			residue_dict = get_residue_dict(path_to_backbone_info,pdb_name)
+
+			## residues along the strand
+			sheet_dict = json.load(open(path_to_json_files+"along/"+pdb_name+"_"+chain+".json","rb+"))
+			along_curv_dict = curvature_values_along(pdb_name,chain,sheet_dict,residue_dict)
+
+			with open(path_to_output+"along/"+pdb_name+"_"+chain+".json","wb+") as f :
+
+				json.dump(along_curv_dict,f)
+			## residues across the strand
+			sheet_dict = json.load(open(path_to_json_files+"across/"+pdb_name+"_"+chain+".json","rb+"))
+		
+			across_curv_dict = curvature_values_across(pdb_name,chain,sheet_dict,residue_dict)
+			with open(path_to_output+"across/"+pdb_name+"_"+chain+".json","wb+") as f :
+
+				json.dump(across_curv_dict,f)
+
+
+
+		except Exception as e :
+			print e
+			error+=1
+			error_file.write(str(e)+"\t"+pdb_name+"\t"+chain+"\n")
+			continue
+
+
+		count+=1
+		
+
+
+	print error,count
+
+if __name__ == "__main__" :
+
+	##path_to_backbone_info = "allBeta_data/backbone_info/"
+	#path_to_json_files = "allBeta_data/sheets/"
+	#pdb_name = "2ich"
+	#chain = "A"
+	#residue_dict = get_residue_dict(path_to_backbone_info,pdb_name)
+
+			## residues along the strand
+	#sheet_dict = json.load(open(path_to_json_files+"along/"+pdb_name+"_"+chain+".json","rb+"))
+	#along_curv_dict = curvature_values_across(pdb_name,chain,sheet_dict,residue_dict)
+
+
+	recurse_pdbs()
